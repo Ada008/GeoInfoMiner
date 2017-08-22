19076da05bbe7abfd035338f1e02b8406384d1f5
jingge326 <jingge.xiao@gmail.com>
2017-08-22 17:26:20 +0800

    5

--- code/svm_stack_mean.py
+++ code/svm_stack_mean.py
@@ -13,6 +13,7 @@ algorithm on Landsat 8 Surface Reflectance data.
 
 from sklearn import svm
 import sys
+import time
 
 # Set the path of source files
 sys.path.append(r'E:\Research\basic\github\GeoInfoMiner\code')
@@ -23,13 +24,13 @@ from imp import reload
 reload(utils)
 
 # Set the path of training samples
-train_file_path = r'E:\Research\basic\github\GeoInfoMiner\data\training_sample.csv'
+train_file_path = r'E:\Research\LandCoverMapping\Experiment\qianshan\Final_2\Classification\roi_and_csv_files\2_stack_mean\Samples_cord.csv'
 
 # Set the path of image that is going to be classified
-imagery_path = r'E:\Research\basic\github\GeoInfoMiner\data\image_sample.tif'
+imagery_path = r'E:\Research\LandCoverMapping\Experiment\qianshan\Final_2\Classification\ImageData\out\com_nor_stack_mean.tif'
 
 # Set the path where to output the final classified image
-class_file_path = r'E:\Research\basic\github\GeoInfoMiner\data\class_svm_stack_mean.tif'
+class_file_path = r'E:\Research\LandCoverMapping\Experiment\qianshan\Final_2\Classification\ImageData\out\class_svm_stack_mean.tif'
 
 # set the split points according to csv files for training
 split_points = [2, 8]
@@ -41,11 +42,18 @@ data_tuple = utils.prepare_data(imagery_path, train_file_path, split_points)
     
 # Train the classifier
 clf_svm = svm.SVC()
-clf_svm.fit(data_tuple[0], [1])
+
+t1 = time.time()
+clf_svm.fit(data_tuple[0], data_tuple[1])
+t2 = time.time()
+print("svm_stack_mean fitting time: " + str(t2 - t1))
 
 # Predict lables based on image data
-z_ps=clf_svm.predict(data_tuple[4])
+t3 = time.time()
+z_ps=clf_svm.predict(data_tuple[2][0])
+t4 = time.time()
+print("svm_stack_mean predicting time: " + str(t4 - t3))
 
 # Output the classification result
-utils.output_lables_to_tif(z_ps, class_file_path, data_tuple[2], data_tuple[3], 
-                           data_tuple[5], data_tuple[6], 1)
+utils.output_lables_to_tif(z_ps, class_file_path, data_tuple[2][1], data_tuple[2][2], 
+                           data_tuple[2][3], data_tuple[2][4], 1)
